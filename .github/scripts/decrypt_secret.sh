e7c0fe40f2b33b1ebda9f6fe25a79c43b70195cc
ArtB <artik222012@gmail.com>
2021-04-15 02:16:35 +0500

    fix workflow

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -4,7 +4,7 @@
 # --batch to prevent interactive command --yes to assume "yes" for questions
 
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output ./8257B447.gpg ./.github/secrets/8257B447.gpg.gpg
+--output ./library/8257B447.gpg ./.github/secrets/8257B447.gpg.gpg
 
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
 --output ./local.properties ./.github/secrets/local.properties.gpg
